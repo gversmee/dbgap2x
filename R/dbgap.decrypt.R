23edda97bb0bea0654479a8c30b945e57b839fe4
gversmee <34459351+gversmee@users.noreply.github.com>
2018-10-18 15:53:35 -0400

    no message

--- R/dbgap.decrypt.R
+++ R/dbgap.decrypt.R
@@ -19,10 +19,11 @@
     key <- gsub(" ", "\\\\ ", key)
 
     # docker run sratools
-    system2("docker", c("run -v", paste0(files, ":/files"), "--rm -it -d --name sratools gversmee/sratoolkit"))
+    system2("docker", c("run --rm -it -d --name sratools gversmee/sratoolkit"))
 
     # copy key to the container
     system(paste("docker cp", key, "sratools:/key.ngc"))
+    system(paste("docker cp", files, "sratools:/files"))
 
     # import the key
     system("docker exec sratools vdb-config --import key.ngc")
@@ -32,6 +33,9 @@
 
     # decrypt
     system(paste("docker exec -w ", wdc, 'sratools sh -c "vdb-decrypt /files"'))
+
+    # copy to host
+    system(paste0("docker cp sratools:/files", paste0(" ", files, "/decrypted_files")))
 
     # rm container
     system("docker stop sratools")
