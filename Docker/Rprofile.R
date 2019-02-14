466c24189016fc12cbf7a9e2d3915e153deb16e2
gversmee <34459351+gversmee@users.noreply.github.com>
2019-02-14 13:59:43 -0500

    no message

--- Docker/Rprofile.R
+++
@@ -1,4 +0,0 @@
-options(unzip = 'internal')
-.libPaths(c('/opt/conda/lib/R/library', '/usr/local/spark-2.3.1-bin-hadoop2.7/R/lib'))
-library(devtools)
-library(parallel)
--- Docker/jupyter_notebook_config.py
+++
@@ -1,8 +0,0 @@
-c = get_config()
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 8888
-
-c.FileContentsManager.delete_to_trash = False
-
-c.IPKernelApp.pylab = 'inline'
-c.NotebookApp.password = 'sha1:48b3bade9809:6e4fca934155cb2552e409a895a22534ff61a837'
