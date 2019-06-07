e7f1dfb29da5b942e5c9fc083b69c8847a58b66b
Mikaunix <mikaunix@gmail.com>
2019-06-07 11:14:08 -0400

    caching in package environment, and cache invalidated every day

--- R/cache.call.R
+++ R/cache.call.R
@@ -2,34 +2,44 @@
 #' @import
 #' @export
 
+cacheEnv <- new.env()
+
 cache.call = function(fname, arg, expr){
     ## transform the "call" object to string
-    fname=paste0(tail(as.character(fname), n=1), collapse = "_")
-    
-    if(!exists("cache")) cache <<- list()
+    fname=paste0("env_", paste0(tail(as.character(fname), n=1), collapse = "_"))
+
     substitute(expr)
 
-    ## only works for single character argument
+    is.ok <- function(e) difftime(Sys.time(), t,units="days") >= 1
+    
+    ## only works for character or numeric arguments
     if(! (all(class(arg) %in% c("character", "numeric"))) ){
         ans <- eval(expr)
     }else{
-        arg = paste0(arg, collapse = "_")
+        ans = NULL
+        arg = paste0(fname, "_", paste0(arg, collapse = "_"))
+        ## print(arg)
         ## check for existence in cache
-        if( is.null(cache[[fname]]) ){
-            cache[[fname]] <<- list()
-        }else{
+        if(exists(arg, envir = cacheEnv)) {
+            element = get(arg, cacheEnv)
+            ## print(element)
             ## check if value is present
-            if ( ! is.null( cache[[fname]][[arg]] ) ){
+            if ( (! is.null(element)) && is.ok(element) ){
                 ## if yes, return it
-                ans <- cache[[fname]][[arg]]
+                ans <- element$value
             }
         }
         
-        if(!exists("ans", environment())){
+        if(is.null(ans)){
             ## if not in cache, evaluate expr
             ans <- eval(expr)
-            ## store it
-            cache[[fname]][[arg]] <<- ans
+            
+            ## store it            
+            assign(arg,
+                   list(
+                       value = ans,
+                       date = Sys.time()),
+                   cacheEnv)
         }    
     }
     
